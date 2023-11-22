769b995bcdd7127ddcbbf31d17aaddd7345c90d9
M-Umwari <m.umwari@alustudent.com>
2023-11-22 20:24:35 +0200

    reactt

--- mental-health-platform/src/App.js
+++ mental-health-platform/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
+import Login from './components/login';
 
 function App() {
   return (

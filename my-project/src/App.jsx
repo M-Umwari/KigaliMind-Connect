cf7c0022c3bd1c3a369811f7d79bc097cf31ad60
M-umwari <m.umwari@alustudent.com>
2023-11-23 13:38:00 +0200

    feat: style the homepage

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -4,11 +4,13 @@ import Support from './components/Support'
 import Login from './components/Login'
 import Topics from './components/Topics';
 import Resources from './components/Resources';
+import Home from './components/Home';
 
 export default function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route>
       <Route path="/login" element={<Login />} />
+      <Route path='/home' element={<Home/>} />
       <Route path="/support" element={<Support />} />
       <Route path="/resources" component={<Resources/>} />
       <Route path="/topics" element={<Topics />}>

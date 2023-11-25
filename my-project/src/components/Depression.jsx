dcae0ced0e5fe5baefabeb35a96aacaf44b6aae4
M-Umwari <m.umwari@alustudent.com>
2023-11-25 15:52:43 +0200

    feat: the chatboard desing link

--- my-project/src/components/Depression.jsx
+++ my-project/src/components/Depression.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 
 export default function Depression() {
   return (
-    <div>
-      Depression links goes here
+    <div className="flex flex-col gap-4">
+      <div>
+        Depression links goes here
+      </div>
+      <Link to="/chatboard" className="bg-blue-950 w-52 text-center text-white hover:bg-blue-300 hover:font-bold hover:text-blue-950 hover: rounded-md px-6 py-3">Join your peers</Link>
     </div>
   )
 }
--- my-project/src/components/Topics.jsx
+++ my-project/src/components/Topics.jsx
@@ -32,14 +32,17 @@ const Topics = () => {
           </div>
         </div>
       </header>
-      <h2>Topics</h2>
-      <section className='grid grid-cols-3 gap-5 '>
-        <Link to="/topics/depression" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >Depression</Link>
-        <Link to="/topics/anxiety" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >Anxiety</Link>
-        <Link to="/topics/loneliness" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >Loneliness</Link>
-        <Link to="/topics/adhd" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >ADHD</Link>
-        <Link to="/topics/stress" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >Stress</Link>
-        <Link to="/topics/eating-disorder" className="bg-red-800 h-32 w-full text-center py-14 rounded-lg" >Eating Disorders</Link>
+      <section className='flex flex-col gap-4 my-10 px-24'>
+        <h2 >Topics</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt adipisci, saepe autem non culpa eaque animi nihil unde repudiandae esse voluptatem officia iure nemo consectetur accusamus quaerat aut at error aliquid dolore rerum ratione! Qui, saepe optio. Praesentium dignissimos ex in odio iusto ipsa sed consectetur necessitatibus libero omnis! Itaque natus porro, odio nihil nesciunt fuga voluptatum animi minima dolorum perferendis mollitia recusandae blanditiis autem voluptatibus soluta iusto sint, ullam et repellendus consequuntur tempora, fugiat exercitationem! At atque consequatur dignissimos dicta repellat culpa nam. Voluptate natus, est nostrum minus expedita culpa autem quibusdam eos illum amet! Reiciendis consectetur accusamus mollitia.</p>
+        <section className='grid grid-cols-3 gap-5 text-red-950 font-bold text-lg'>
+          <Link to="/topics/depression" className="bg-[url('src/assets/Images/Anxiety.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >Depression</Link>
+          <Link to="/topics/anxiety" className="bg-[url('src/assets/Images/Anxiety.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >Anxiety</Link>
+          <Link to="/topics/loneliness" className="bg-[url('src/assets/Images/loneliness.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >Loneliness</Link>
+          <Link to="/topics/adhd" className="bg-[url('src/assets/Images/Anxiety.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >ADHD</Link>
+          <Link to="/topics/stress" className="bg-[url('src/assets/Images/Anxiety.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >Stress</Link>
+          <Link to="/topics/eating-disorders" className="bg-[url('src/assets/Images/Anxiety.svg')] bg-auto bg-no-repeat h-32 w-full text-center py-14 rounded-lg" >Eating Disorders</Link>
+        </section>
       </section>
     </div>
   );

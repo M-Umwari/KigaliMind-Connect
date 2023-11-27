de529481b4bcb1084d437d3d159da0f49f39ffd1
M-Umwari <m.umwari@alustudent.com>
2023-11-27 12:33:28 +0200

    Header placements

--- my-project/src/components/Resources.jsx
+++ my-project/src/components/Resources.jsx
@@ -1,4 +1,4 @@
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import { auth } from '../services/authentication';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
@@ -6,6 +6,33 @@ export default function Resources() {
   const navigate = useNavigate()
   return (
     <div>
+      <header className="bg-blue-950 text-blue-300 px-20 h-1/4">
+        {/* Logo */}
+        <div className="max-w-7xl mx-auto flex items-center h-full justify-between ">
+          <div className=" rounded-full my-3 ">         
+             <img src='src/assets/Images/logo.png' className=" h-60 rounded-full" />
+          </div>
+          <div className=" flex gap-4 items-center md:text-2xl">
+            {/* Navigation Links */}
+            <NavLink to="/topics" className={({ isActive }) =>
+              isActive
+                ? "flex h-10 items-center gap-1 px-4 underline"
+                : "flex h-10 items-center gap-1.5 px-4 "}>Topics</NavLink>
+            <NavLink to="/resources" className={({ isActive }) =>
+              isActive
+                ? "flex h-10 items-center gap-1 px-4 d underline"
+                : "flex h-10 items-center gap-1.5 px-4 "}>Resources</NavLink>
+            <NavLink to="/support" className={({ isActive }) =>
+              isActive
+                ? "flex h-10 items-center gap-1 px-4  underline"
+                : "flex h-10 items-center gap-1.5 px-4 "}>Support</NavLink>
+            <NavLink to="/chatboard" className={({ isActive }) =>
+              isActive
+                ? "flex h-10 items-center gap-1 px-4  underline"
+                : "flex h-10 items-center gap-1.5 px-4 "}>Chatboard</NavLink>
+          </div>
+        </div>
+      </header>
       <footer className="flex  items-center gap-20 justify-center bg-blue-950 text-blue-300 fixed bottom-0 h-10 w-full">
         <p>RWANDA MIND CONNECT</p>
         <div className="flex gap-4">
